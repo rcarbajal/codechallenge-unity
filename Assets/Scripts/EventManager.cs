bb5bb49eddae01f12f6ae9213a0a36983930682b
Robert Carbajal <robert@holycow2.com>
2021-10-31 00:04:17 -0500

    - Add initial project setup that includes side scrolling, initial assets, player idle animation

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class EventManager : MonoBehaviour
+{
+    public static class Events
+    {
+		public static readonly string RECYCLE_GROUND = "recycle_ground";
+    } //end inner class Events
+
+    private Dictionary<string, UnityEvent> eventDictionary;
+
+    private static EventManager eventManager;
+
+    private static EventManager Instance
+    {
+        get
+        {
+            if (!eventManager)
+            {
+                eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+                if (!eventManager)
+                    Debug.LogError("No active EventManager component available in the scene.");
+                else
+                    eventManager.Init();
+            } //end if
+
+            return eventManager;
+        } //end get
+    } //end Instance property
+
+    #region Private methods
+
+    private void Init()
+    {
+        if (eventDictionary == null)
+            eventDictionary = new Dictionary<string, UnityEvent>();
+    } //end method Init
+
+    #endregion
+
+    #region Public static methods
+
+    public static void AddEventListener(string eventName, UnityAction listener)
+    {
+		if (Instance.eventDictionary.TryGetValue(eventName, out UnityEvent thisEvent))
+			thisEvent.AddListener(listener);
+		else
+		{
+			thisEvent = new UnityEvent();
+			thisEvent.AddListener(listener);
+			Instance.eventDictionary.Add(eventName, thisEvent);
+		} //end else
+    } //end method AddEventListener
+
+    public static void RemoveEventListener(string eventName, UnityAction listener)
+    {
+        if (eventManager == null) return;
+
+        if (Instance.eventDictionary.TryGetValue(eventName, out UnityEvent thisEvent))
+            thisEvent.RemoveListener(listener);
+    } //end method RemoveEventListener
+
+	public static void RemoveAllListeners(string eventName)
+	{
+		if (eventManager == null) return;
+
+		if (Instance.eventDictionary.TryGetValue(eventName, out UnityEvent thisEvent))
+			thisEvent.RemoveAllListeners();
+	} //end method RemoveAllListeners
+
+    public static void Broadcast(string eventName)
+    {
+		if (Instance.eventDictionary.TryGetValue(eventName, out UnityEvent thisEvent))
+			thisEvent.Invoke();
+    } //end method Broadcast
+
+    #endregion
+} //end class EventManager
---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+	private class GroundQueueHolder
+	{
+		public GameObject GroundObject { get; set; }
+		public Queue<GameObject> OriginQueue { get; set; }
+	} //end class GroundQueueHolder
+
+	private static MainController Instance;
+
+	[Header("Player")]
+	[SerializeField] private Transform Player;
+
+	[Header("Ground objects")]
+	[SerializeField] private GameObject[] FlatGrounds;
+	[SerializeField] private GameObject[] SmallHills;
+	[SerializeField] private GameObject[] TallHills;
+
+	private Queue<GameObject> flatQueue;
+	private Queue<GameObject> smallQueue;
+	private Queue<GameObject> tallQueue;
+	private Queue<GroundQueueHolder> mainQueue;
+
+	private bool isGroundPlaced = false;
+
+	private System.Random rnd;
+
+	#region Monobehaviour methods
+
+	private void Awake()
+	{
+		Instance = this;
+	} //end method Awake
+
+	private void Start()
+	{
+		/*
+		 * collect ground prefab instances into queues
+		 */
+		flatQueue = new Queue<GameObject>();
+		if (FlatGrounds != null && FlatGrounds.Length > 0)
+			foreach (GameObject obj in FlatGrounds)
+			{
+				flatQueue.Enqueue(obj);
+				obj.SetActive(false);
+			} //end foreach
+
+		smallQueue = new Queue<GameObject>();
+		if (SmallHills != null && SmallHills.Length > 0)
+			foreach (GameObject obj in SmallHills)
+			{
+				smallQueue.Enqueue(obj);
+				obj.SetActive(false);
+			} //end foreach
+
+		tallQueue = new Queue<GameObject>();
+		if (TallHills != null && TallHills.Length > 0)
+			foreach (GameObject obj in TallHills)
+			{
+				tallQueue.Enqueue(obj);
+				obj.SetActive(false);
+			} //end foreach
+
+		//initialize
+		rnd = new System.Random();
+		mainQueue = new Queue<GroundQueueHolder>();
+
+		//pick two ground objects and place them
+		GroundQueueHolder gqh = GetRandomGroundObject();
+		gqh.GroundObject.transform.localPosition = new Vector3(0, 0, 0);
+		gqh.GroundObject.SetActive(true);
+		mainQueue.Enqueue(gqh);
+
+		gqh = GetRandomGroundObject();
+		gqh.GroundObject.transform.localPosition = new Vector3(20, 0, 0);
+		gqh.GroundObject.SetActive(true);
+		mainQueue.Enqueue(gqh);
+
+		isGroundPlaced = true;
+	} //end method Start
+
+	private void FixedUpdate()
+	{
+		int pos = (int)Mathf.Round(Player.transform.localPosition.x);
+
+		if (pos >= 20 && pos % 20 == 0 && !isGroundPlaced)
+		{
+			isGroundPlaced = true;
+
+			//put back ground that has moved out of view
+			GroundQueueHolder gqh = mainQueue.Dequeue();
+			gqh.GroundObject.SetActive(false);
+			gqh.OriginQueue.Enqueue(gqh.GroundObject);
+
+			//get new random ground piece and place in front of player
+			gqh = GetRandomGroundObject();
+			gqh.GroundObject.transform.localPosition = new Vector3(Player.transform.localPosition.x + 20, 0, 0);
+			gqh.GroundObject.SetActive(true);
+			mainQueue.Enqueue(gqh);
+		} //end if
+
+		if ((pos - 10) % 20 == 0)
+			isGroundPlaced = false;
+	} //end method FixedUpdate
+
+	#endregion
+
+	#region Private methods
+
+	private GroundQueueHolder GetRandomGroundObject()
+	{
+		GroundQueueHolder gqh;
+		GameObject obj;
+		int listNum = rnd.Next(1, 4);
+		switch (listNum)
+		{
+			case 1:
+				obj = flatQueue.Dequeue();
+				gqh = new GroundQueueHolder
+				{
+					GroundObject = obj,
+					OriginQueue = flatQueue
+				};
+				break;
+			case 2:
+				obj = smallQueue.Dequeue();
+				gqh = new GroundQueueHolder
+				{
+					GroundObject = obj,
+					OriginQueue = smallQueue
+				};
+				break;
+			case 3:
+				obj = tallQueue.Dequeue();
+				gqh = new GroundQueueHolder
+				{
+					GroundObject = obj,
+					OriginQueue = tallQueue
+				};
+				break;
+			default:
+				obj = flatQueue.Dequeue();
+				gqh = new GroundQueueHolder
+				{
+					GroundObject = obj,
+					OriginQueue = flatQueue
+				};
+				break;
+		} //end switch
+
+		return gqh;
+	} //end method GetRandomGroundObject
+
+	#endregion
+} //end class MainController
